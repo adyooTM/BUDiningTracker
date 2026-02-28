273c91d253ec29bc8b367ef9ae7668ad098b7d0d
Andy Yoo <andyyoo@crc-dot1x-nat-10-239-39-26.bu.edu>
2026-02-28 03:05:25 -0500

    Update

---
+++ BUdiningtracker/BUdiningtracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BUdiningtracker
+//
+//  Created by Andy Yoo on 2/28/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ flutterfiles/android/app/src/main/kotlin/com/example/flutterfiles/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterfiles
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
